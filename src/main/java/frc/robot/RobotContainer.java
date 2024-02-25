eada4ccf34a1cc5983d7872a47b651a8a5a596eb
Pangolin-Bryan <bryan0605bsc@gmail.com>
2024-02-25 11:45:22 +0800

    Clean UP

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.ShooterConstants;
 import frc.robot.commands.ClimbCommand;
 import frc.robot.commands.EjectNoteIdelPose;
